e6f0c2aeafdc7331c082c9cd3e4ff97020ba9e99
GustavoRolo <gustavohenriquesantosrolo@gmail.com>
2022-05-18 13:57:52 -0300

    adicionando pág sobre

--- WebFotovoltaico/server/app.js
+++ WebFotovoltaico/server/app.js
@@ -9,4 +9,16 @@ app.get("/", function(req, res) {
     res.sendFile(resolve('./main/index.html'));
 });
 
+app.get("/consumo", function(req, res) {
+    res.sendFile(resolve('./main/consumo.html'));
+});
+
+app.get("/fornecedores", function(req, res) {
+    res.sendFile(resolve('./main/fornecedores.html'));
+});
+
+app.get("/sobre", function(req, res) {
+    res.sendFile(resolve('./main/sobre.html'));
+});
+
 http.createServer(app).listen(3000, () => console.log("Servidor rodando local na porta 3000"));
